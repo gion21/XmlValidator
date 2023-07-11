9ea1afed128419482f0cf99c3b4e23e4709a6d37
Ion Gherasim <ion@gherasim.de>
2023-07-11 12:18:59 +0200

    Revert "Revert "First class""
    This reverts commit b7b53ce14c23a9cb006b9f6fe8029d63e87573a9.

---
+++ src/main/java/de/validator/xml/XmlValidator.java
@@ -0,0 +1,11 @@
+package de.validator.xml;
+
+public class XmlValidator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main");
+
+	}
+
+}
