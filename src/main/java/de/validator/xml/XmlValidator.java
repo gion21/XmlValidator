7438f938c32517f4994073030c9d1cbade98aa8c
Ion Gherasim <ion@gherasim.de>
2023-07-11 12:28:30 +0200

    Clean the main

--- src/main/java/de/validator/xml/XmlValidator.java
+++ src/main/java/de/validator/xml/XmlValidator.java
@@ -4,9 +4,6 @@ public class XmlValidator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Main");
-		System.out.println("Main2 ");
-		System.out.println("Main3");
 
 	}
 
